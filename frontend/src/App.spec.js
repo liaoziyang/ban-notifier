d9f5bd122984c7c1c4c9bcd29f32d6541ab27aef
Emiel Van Severen <emielvanseveren@outlook.com>
2019-09-11 20:57:58 +0000

    test environment with enzyme snapshot test in app

--- frontend/src/App.spec.js
+++ frontend/src/App.spec.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import App from './App'
-
+import { mount } from 'enzyme'
 
 describe('App', () => {
   it('renders correctly', () => {
-    expect('true')
+    expect(mount(createComponent())).toMatchSnapshot()
   })
 })
+
+function createComponent(props){
+  const defaultProps = {}
+  return <App {...defaultProps} {...props}/>
+}
