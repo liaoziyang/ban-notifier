7d61a81e92d70e6e714977a460b0e429d0a14952
Emiel Van Severen <emielvanseveren@outlook.com>
2019-09-11 21:55:07 +0000

    added home page and not found page + routing file

---
+++ frontend/src/components/Routing.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
+// checks if history exists
+const supportsHistory = 'pushState' in window.history
+export default function Routing(){
+  return (
+    <Router forceRefresh={!supportsHistory}>
+      <Switch>
+        <Route component={Home} exact path="/"/>
+        <Route component={NotFound}/>
+      </Switch>
+    </Router>
+  )
+}
---
+++ frontend/src/components/pages/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NotFound(){
+  return (
+    <div>the page could not be found</div>
+  )
+}
