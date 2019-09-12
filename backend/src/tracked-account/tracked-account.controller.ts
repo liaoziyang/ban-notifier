8909df6bc1df11a35c96b170790123564b2cf1f9
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2019-09-12 13:39:40 +0000

    Implement basic validation with pipes for create trackedAccount

--- backend/src/tracked-account/tracked-account.controller.ts
+++ backend/src/tracked-account/tracked-account.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe, Post, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, Delete, Body, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TrackedAccountService } from './tracked-account.service';
 import TrackedAccount from './trackedAccount.entity';
 import { CreateTrackedAccountDto } from './dto/create-tracked-account.dto';
@@ -12,8 +12,9 @@ export class TrackedAccountController {
         return this.trackedAccountService.getTrackedAccountById(id);
     }
 
-    @Post('/')
-    createdTrackedAccount(@Body() createTrackedAccountDto: CreateTrackedAccountDto) {
+    @Post()
+    @UsePipes(ValidationPipe)
+    createdTrackedAccount(@Body() createTrackedAccountDto: CreateTrackedAccountDto): Promise<TrackedAccount> {
         return this.trackedAccountService.createTrackedAccount(createTrackedAccountDto);
     }
 
