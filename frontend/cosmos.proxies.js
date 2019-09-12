6d8acbb917681da9a760c358838897597a9a6ab9
Emiel Van Severen <emielvanseveren@outlook.com>
2019-09-12 13:12:16 +0000

    added cosmos config + proxy file

---
+++ frontend/cosmos.proxies.js
@@ -0,0 +1,11 @@
+import createRouterProxy from 'react-cosmos-router-proxy'
+import StatefulProxy from 'react-cosmos-stateful-proxy'
+import createWrapperProxy from 'react-cosmos-wrapper-proxy'
+
+import { CosmosGlobalStyle } from './src/constants/style'
+
+export default [
+  StatefulProxy,
+  createRouterProxy(),
+  createWrapperProxy({ component: CosmosGlobalStyle, defaultEnabled: true })
+]
