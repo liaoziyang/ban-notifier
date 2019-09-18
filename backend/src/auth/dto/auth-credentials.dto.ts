259a425a2d8477997e8b6a0c55ee2930a94f732d
Emiel Van Severen <emielvanseveren@outlook.com>
2019-09-18 23:38:44 +0000

    added reggex email

--- backend/src/auth/dto/auth-credentials.dto.ts
+++ backend/src/auth/dto/auth-credentials.dto.ts
@@ -1,6 +1,8 @@
 import { MinLength, IsString, MaxLength, Matches } from 'class-validator';
 
 export class AuthCredentialsDto {
+
+    // /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i checks if this is a valid email
     @IsString()
     @MinLength(4)
     @MaxLength(20)
