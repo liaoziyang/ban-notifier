0c4f81e4491ff51ffc54453e941cb0b76687b34d
Emiel Van Severen <emielvanseveren@outlook.com>
2019-09-19 15:14:33 +0000

    cleaner error messages

--- frontend/src/components/auth/signupSchema.js
+++ frontend/src/components/auth/signupSchema.js
@@ -2,23 +2,23 @@ import * as Yup from 'yup'
 
 const signupSchema = Yup.object().shape({
   email:    Yup.string()
-    .email('This is not a valid email address')
-    .required('This field is required'),
+    .email('This is not a valid email address.')
+    .required('This field is required.'),
   username: Yup.string()
-    .min(4, 'Too short!')
-    .max(20, 'Too Long!')
+    .min(4, 'Too short.')
+    .max(20, 'Too long.')
     .matches(/([A-Za-z0-9_-]*$)/, 'should only contain letters, numbers, dash or underscores')
-    .required('This field is required'),
+    .required('This field is required.'),
   password: Yup.string()
-    .min(8, 'Too short!')
-    .max(20, 'Too long!')
-    .matches(/(?=.*\W+)/g, 'should contain a special char')
-    .matches(/(?=.*[A-Z])/g, 'should contain a capital letter')
-    .matches(/(?=.*[a-z].*$)/g,'should contain a small letter')
-    .matches(/(?![.\n])/g, 'should contain a number')
-    .required('This field is required'),
+    .min(8, 'Too short.')
+    .max(20, 'Too long.')
+    .matches(/(?=.*\W+)/g, 'Should contain a special char.')
+    .matches(/(?=.*[A-Z])/g, 'Should contain a capital letter.')
+    .matches(/(?=.*[a-z].*$)/g,'Should contain a small letter.')
+    .matches(/(?![.\n])/g, 'Should contain a number.')
+    .required('This field is required.'),
   repeatPassword: Yup.string()
-    .oneOf([Yup.ref('password'), null], 'passwords must match.').required('This field is required')
+    .oneOf([Yup.ref('password'), null], 'passwords must match.').required('This field is required.')
 })
 
 export default signupSchema
